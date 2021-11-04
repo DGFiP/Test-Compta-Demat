a043c476b9d111a5ee0bf96fecd1d572c747ae3f
compta-demat <71333035+compta-demat@users.noreply.github.com>
2021-11-04 10:21:00 +0100

    Add files via upload

--- src/testeur/trt_xml.pl
+++ src/testeur/trt_xml.pl
@@ -426,6 +426,10 @@ sub constructionPlat {
                 . "donnee code : "
                 . $plat_code[$indice_xml] );
         $txt[$indice_txt] = $plat[$indice_xml];
+       # if ( $plat[$indice_xml] !~ m/^[\+\-]*[0-9]{1,}\.*[0-9]*[\+\-]*$/ ) {
+        if ( $plat[$indice_xml] =~ m/^[\+\-]?[ ]*([0-9]*)\.(([0-9])*)[ ]*$/ ) {
+            $txt[$indice_txt]  =~ tr/./,/;
+        }
     }
 
   #RG:F: retraitement debit / credit par ligne en présence de Montant/sens :I
@@ -433,11 +437,13 @@ sub constructionPlat {
     if ( defined $plat[$pos_sens] && defined $plat[$pos_montant] ) {
         &trace( "Montant : " . $plat[$pos_montant] . " SENS :" . $plat[$pos_sens] );
         if ( uc( $plat[$pos_sens] ) eq "C" || $plat[$pos_sens] =~ /^\s*\-1\s*$/ ) {
+            $plat[$pos_montant] =~ tr/./,/;
             $txt[10] = $plat[$pos_montant];
             $txt[9]  = 0;
 
         }
         elsif ( uc( $plat[$pos_sens] ) eq "D" || $plat[$pos_sens] =~ /^\s*\+1\s*$/ ) {
+            $plat[$pos_montant] =~ tr/./,/;
             $txt[9]  = $plat[$pos_montant];
             $txt[10] = 0;
         }
@@ -651,8 +657,10 @@ sub reglesMetier {
         }
     }
     elsif ( $metier eq 'NU' ) {
+    #2021 forcage fichier xml separateur , pour compatibilite fichiers plats
         $valeurSortie =~ tr/,/./;
         if ( $valeurSortie !~ m/^[\+\-]*[0-9]{1,}\.*[0-9]*[\+\-]*$/ ) {
+        #if ( $valeurSortie !~ m/^[\+\-]*[0-9]{1,},*[0-9]*[\+\-]*$/ ) {
             &trace( "Somme incorrecte $valeurEntree \n", "X" );
         }
 
