b4c45565e070f66f4ef653daf130d65fd6573c7e
compta-demat <71333035+compta-demat@users.noreply.github.com>
2021-11-04 10:05:47 +0100

    Add files via upload

---
+++ src/testeur/environnement_alto2.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl -- -*- C -*- 
+sub Env_Path { 
+		$ENV{ProgramFiles} = "C:/Users/rbrissaud02/Test-Compta-Demat/src"; 
+		$ENV{ProgramData} = "C:/Users/rbrissaud02/Test-Compta-Demat/src"; 
+} 
+1; #return true 
